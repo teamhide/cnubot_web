b580a274188fb5a41cd3973392b4207aa31a265c
hide <hide@hides.kr>
2018-06-05 04:03:21 +0900

    After Work hour

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './index.css';
 
 class index extends Component {
     render() {
         return (
-            <div>
-                footer
+            <div className="footer">
+                <p>© Copyright 2018 Hide</p>
+                <p>카카오톡 플러스친구 <b><a href="https://pf.kakao.com/_Ifxerxl" target="_blank">충남대학식알림봇</a></b></p>
             </div>
         );
     }
--- src/components/Section/Modal.js
+++ src/components/Section/Modal.js
@@ -4,11 +4,25 @@ import './Modal.css';
 class Modal extends Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
         this.state = {
             place: '',
             student1: '',
-            student2: '',
+            student2: `
+            <span>월요일</span>
+            <span className="workhour">오전 11:30 ~ 오후 02:00</span>
+            <span>화요일</span>
+            <span className="workhour">오전 11:30 ~ 오후 02:00</span>
+            <span>수요일</span>
+            <span className="workhour">오전 11:30 ~ 오후 02:00</span>
+            <span>목요일</span>
+            <span className="workhour">오전 11:30 ~ 오후 02:00</span>
+            <span>금요일</span>
+            <span className="workhour">오전 11:30 ~ 오후 02:00</span>
+            <span>토요일</span>
+            <span className="workhour">영업 안함</span>
+            <span>일요일</span>
+            <span className="workhour">영업 안함</span>
+            `,
             student3: '',
             sangrok: '',
             science: '',
@@ -16,14 +30,87 @@ class Modal extends Component {
         }
     }
     _renderWorkHour = (place) => {
-
+        console.log(place);
+        let workHour = "";
+        if(place === "제2학생회관" || place === "제3학생회관" || place === "상록회관" || place === "생활과학대학") {
+            workHour = `
+            <span>월요일</span>
+            <span>오전 11:30 ~ 오후 02:00</span>
+            <span>화요일</span>
+            <span>오전 11:30 ~ 오후 02:00</span>
+            <span>수요일</span>
+            <span>오전 11:30 ~ 오후 02:00</span>
+            <span>목요일</span>
+            <span>오전 11:30 ~ 오후 02:00</span>
+            <span>금요일</span>
+            <span>오전 11:30 ~ 오후 02:00</span>
+            <span>토요일</span>
+            <span>영업 안함</span>
+            <span>일요일</span>
+            <span>영업 안함</span>
+            `;
+        } else if(place === "기숙사") {
+            workHour = `
+            <span class="dorm-header">월요일</span>
+            <span>오전 07:00 ~ 오전 09:00</span>
+            <span>오전 11:30 ~ 오후 01:30</span>
+            <span>오후 05:30 ~ 오후 07:30</span>
+            <span class="dorm-header">화요일</span>
+            <span>오전 07:00 ~ 오전 09:00</span>
+            <span>오전 11:30 ~ 오후 01:30</span>
+            <span>오후 05:30 ~ 오후 07:30</span>
+            <span class="dorm-header">수요일</span>
+            <span>오전 07:00 ~ 오전 09:00</span>
+            <span>오전 11:30 ~ 오후 01:30</span>
+            <span>오후 05:30 ~ 오후 07:30</span>
+            <span class="dorm-header">목요일</span>
+            <span>오전 07:00 ~ 오전 09:00</span>
+            <span>오전 11:30 ~ 오후 01:30</span>
+            <span>오후 05:30 ~ 오후 07:30</span>
+            <span>금요일</span>
+            <span>오전 07:00 ~ 오전 09:00</span>
+            <span>오전 11:30 ~ 오후 01:30</span>
+            <span>오후 05:30 ~ 오후 07:30</span>
+            <span>토요일</span>
+            <span>오전 07:30 ~ 오전 09:00</span>
+            <span>오전 11:30 ~ 오후 01:30</span>
+            <span>오후 05:30 ~ 오후 07:00</span>
+            <span>일요일</span>
+            <span>오전 07:30 ~ 오전 09:00</span>
+            <span>오전 11:30 ~ 오후 01:30</span>
+            <span>오후 05:30 ~ 오후 07:00</span>
+            `
+        } else if(place === "제1학생회관") {
+            workHour = `
+            <span>월요일</span>
+            <span>오전 11:00 ~ 오후 07:00</span>
+            <span>화요일</span>
+            <span>오전 11:00 ~ 오후 07:00</span>
+            <span>수요일</span>
+            <span>오전 11:00 ~ 오후 07:00</span>
+            <span>목요일</span>
+            <span>오전 11:00 ~ 오후 07:00</span>
+            <span>금요일</span>
+            <span>오전 11:00 ~ 오후 07:00</span>
+            <span>토요일</span>
+            <span>오전 11:00 ~ 오후 02:00</span>
+            <span>일요일</span>
+            <span>영업하지 않습니다</span>
+            `
+        } else {
+            workHour = "Error"
+        }
         return (
             <div className="modal-container">
                 <div className="modal-header">
                     { this.props.place ? this.props.place : "Loading" }
                 </div>
                 <div className="modal-body">
-                    { this.props.hour ? this.props.hour : "Loading"}
+                    { workHour ? (
+                        <div>
+                            <div dangerouslySetInnerHTML={ {__html: workHour} }></div>
+                        </div>
+                    ) : "Loading" }
                 </div>
                 <div className="modal-footer">
                     <button type="button" onClick={this.props.closeFunc}>닫기</button>
@@ -34,7 +121,7 @@ class Modal extends Component {
     render() {
         return (
             <div className="modal">
-                { this._renderWorkHour() }
+                { this._renderWorkHour(this.props.place) }
             </div>
         );
     }
