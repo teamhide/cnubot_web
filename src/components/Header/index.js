6c786d7dcd7e75e08dc63fde2fad32b4e876ae00
hide <hide@hides.kr>
2018-06-06 00:12:02 +0900

    Add Hamburger menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,18 @@ class index extends Component {
     render() {
         return (
             <header>
-                <div className="header-logo">
-                    <NavLink to="/">CNUBAB</NavLink>
+                <div className="pc-menu">
+                    <div className="header-logo">
+                        <NavLink to="/">CNUBAB</NavLink>
+                        <label for="toggle">&#9776;</label>
+                    </div>
+                    <div className="header-menu">
+                        <NavLink to="/">MENU</NavLink>
+                        <NavLink to="/about">ABOUT</NavLink>
+                    </div>
                 </div>
-                <div className="header-menu">
+                <input type="checkbox" id="toggle" />
+                <div className="mobile-menu">
                     <NavLink to="/">MENU</NavLink>
                     <NavLink to="/about">ABOUT</NavLink>
                 </div>
